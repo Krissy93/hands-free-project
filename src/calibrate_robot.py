1f72f96867a8144fcdf14389a477dee7e76aede5
Jacopo Micheletti <j.micheletti@studenti.unibs.it>
2024-08-26 16:34:56 +0200

    Correzione file calibrazione

--- src/calibrate_robot.py
+++ src/calibrate_robot.py
@@ -10,8 +10,7 @@ from geometry_msgs.msg import PoseStamped
 
 
 def calibrate(ws1, ws2):
-    """
-    Function to calibrate two workspaces using the same number of points
+    ''' Function to calibrate two workspaces using the same number of points
     which coordinates are expressed according to the first and second reference system.
     The result is the rototranslation matrix Rt used to convert the point from ws1 to ws2 coordinates.
     According to the number of points coordinates, the resulting R is shaped correctly.
@@ -26,18 +25,59 @@ def calibrate(ws1, ws2):
     OUTPUTS:
     - R: rototranslation matrix containing rotation and translation parameters,
          plus a row of zeros and a 1 in the bottom-right corner
-    """
+    '''
 
+    # convert points from ws1 to numpy array
     A = np.asarray(ws1)
-    A = np.append(A, np.ones((A.shape[0], 1)), axis=1)
+    # each point should be expressed as homogeneous coordinates, aka adding a 1 at the end
+    A = np.append(A, np.ones((A.shape[0],1)), axis=1)
+
+    # define how to swap coordinates according to reference
+    # basically we create a rule to swap coordinates by making the second tuple of
+    # ref equal to the first
+
+    # b is the vector containing the ws2 coordinates
+    # in this case coordinates do not need to be converted to homogeneous
     b = np.asarray(ws2)
+    # ws2_ = np.asarray(ws2)
+    # b = ws2_.copy()
+    # for i in range(0, len(ref[0])):
+    #     try:
+    #         # the coordinate is found with the same sign
+    #         idx = ref[1].index(ref[0][i])
+    #         b[:,i] = ws2_[:,idx]
+    #     except:
+    #         # in this case the coordinate swaps sign
+    #         idx = ref[1].index(-ref[0][i])
+    #         b[:,i] = -ws2_[:,idx]
 
-    x, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+    # solve linear system x = A\b
+    # the result is a vector x of values that must be placed in the correct spots
+    # of the rototranslation matrix by hand, namely r11 r12 r13 r21 r22 r23 r13 r23 r33 t1 t2 t3
+    x = np.linalg.lstsq(A,b,rcond=None)
+    # x is now an array of shape (4,1) where only the first element contains the actual
+    # parameters of Rt! So we need to convert the result (only first element) to a list
+    x = x[0].flatten()
+    x = x.tolist()
 
-    if x.shape[0] > 3:
-        R = np.vstack([x.T, [0, 0, 0, 1]])
+    # define rototranslation matrix using the values of x
+    # it may be different according to the length of points (aka if the third coordinate
+    # is not present the matrix only has r11 r12 r21 r22 t1 t2)
+    # | r11 r12 r13 tx |
+    # | r21 r22 r23 ty |
+    # | r31 r32 r33 tz |
+    # |  0   0   0   1 |
+    #R = [[-x[1], x[0], -x[2]], [x[0], x[1], x[3]], [0.0, 0.0, 1.0]]
+
+    # note that solving a linear system means that resulting coefficients are found first,
+    # this is why all translation parameters (which are only scalars and not multiplied
+    # for variables) are last in the resulting vector
+    if len(x) > 6:
+        R = [[x[0], x[1], x[2], x[9]], [x[3], x[4], x[5], x[10]], [x[6], x[7], x[8], x[11]], [0.0, 0.0, 0.0, 1.0]]
     else:
-        R = np.vstack([np.hstack([x.T, np.zeros((3, 1))]), [0, 0, 0, 1]])
+        R = [[x[0], x[1], x[4]], [x[2], x[3], x[5]], [0.0, 0.0, 1.0]]
+    # convert R from list to numpy array
+    R = np.asarray(R)
 
     return R
 
@@ -95,13 +135,10 @@ def main(H_master_yaml, W_master_yaml, calibration_yaml):
     markers_H = dict_H['Markers']
 
     # Access the 'Master', 'Robot', and 'Calibration' from dict_W
-    dict_W_master = dict_W[0]['Master']
-    dict_W_robot = dict_W[1]['Robot']
-    dict_W_calibration = dict_W[2]['Calibration']
-
-    pose_W = dict_W_master
-    markers_W = dict_W_master
+    pose_W = dict_W['Pose']
+    markers_W = dict_W['Markers']
     
+    # only acquires a position when ENTER is pressed, allowing to move the robot in manual guidance
     points = []
 
     i = 0
